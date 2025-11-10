d8028526a1e5ed47563a7c5870bead440c83ca80
patrickaigbogun <rjtekdom@gmail.com>
2025-11-10 18:00:19 +0100

    minor

--- routes/api.php
+++ routes/api.php
@@ -10,3 +10,7 @@ Route::get('/user', function (Request $request) {
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+
+Route::get('/ping', function () {
+    return response()->json(['status' => 'ok', 'message' => 'pong'], 200);
+});
